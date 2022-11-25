31bc53b454317b6c6fc8a298a33b9b0a24380064
FatihAYCA <110502174+FatihAYCA@users.noreply.github.com>
2022-11-26 01:02:52 +0300

    Delete deneme.php

--- Ek Ödev 2 - Fatura Uygulaması/deneme.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-// for ($i=0; $i < 4; $i++) { 
-//     $_SESSION["tazeurunkg$i"] = $_POST["tazeurunkg$i"];
-//     $_SESSION["urunkg$i"] = $_POST["urunkg$i"];
-// }
-// var_dump($_SESSION);
-
-?>
